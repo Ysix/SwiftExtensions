62c7cda71ae161ddf79491405250958d311988ca
Iman Zarrabian <iman@omts.fr>
2017-04-03 15:21:26 +0200

    added duration cc @fpoisson and Swift3 compatibility on KeyboardObservation Extentsion

--- UIViewController+KeyboadObservation.swift
+++ UIViewController+KeyboadObservation.swift
@@ -9,32 +9,37 @@
 import UIKit
 
 protocol KeyboardObserverDelegate {
-    func keyboardWillMove(to height: CGFloat)
+    func keyboardWillMove(to height: CGFloat, during duration: TimeInterval)
 }
 
 extension UIViewController {
 
     func startObservingKeyboard() // call it from viewWillAppear
     {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(UIViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(UIViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(UIViewController.keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(UIViewController.keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
 
     func stopObservingKeyboard() // call it from viewWillDissappear
     {
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
 
-    func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue() {
-            // swiftlint:disable:next force_cast
-            (self as! KeyboardObserverDelegate).keyboardWillMove(to: keyboardSize.height)
+    func keyboardWillShow(_ notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue, let keyboardDuration = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval else {
+            return
         }
+        // swiftlint:disable:next force_cast
+        (self as! KeyboardObserverDelegate).keyboardWillMove(to: keyboardSize.height, during: keyboardDuration)
     }
 
-    func keyboardWillHide(notification: NSNotification) {
+    func keyboardWillHide(_ notification: NSNotification) {
+        guard let keyboardDuration = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval else {
+            return
+        }
+        
         // swiftlint:disable:next force_cast
-        (self as! KeyboardObserverDelegate).keyboardWillMove(to: 0)
+        (self as! KeyboardObserverDelegate).keyboardWillMove(to: 0, during: keyboardDuration)
     }
 }
